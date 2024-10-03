2985c6e55c8fd44ff6f90381c9c6f12c3e9c18bf
aastha <aasthasinha2603@gmail.com>
2024-10-03 09:25:18 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum=0;
+    printf("enter the values");
+    scanf("%d,%d",&a,&b);
+    sum = a+b;
+    printf("the sum of two integers is %d",sum);
+    return 0;
+    
+}
